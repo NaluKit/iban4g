75852cdf54bc8717f384492969d2f99c180b62dd
Frank Hossfeld <frank.hossfeld@googlemail.com>
2017-09-13 09:38:46 +0200

    update comments

--- src/main/java/de/gishmo/gwt/iban4g/package-info.java
+++ src/main/java/de/gishmo/gwt/iban4g/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Root package for iban4j implementation.
+ * Root package for iban4g implementation.
  * Provides a builder class and utilities to generate Iban, calculate check digit and validate it.
  * Provides utilities to generate Bic and validate it.
  * Includes exceptions for Iban and Bic validation.
--- src/main/java/de/gishmo/gwt/iban4g/shared/bban/package-info.java
+++ src/main/java/de/gishmo/gwt/iban4g/shared/bban/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Bban classes for iban4j implementation.
+ * Bban classes for iban4g implementation.
  */
 package de.gishmo.gwt.iban4g.shared.bban;
